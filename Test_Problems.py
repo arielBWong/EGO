c9ea68a2ed897bba532e32aa17160d0c176658ea
arielBWong <ariel.bingwang@outlook.com>
2019-12-02 03:32:18 +0800

    test problem adding contour plot, but the testing problem needs a new one for meshed x and y

--- Test_Problems.py
+++ Test_Problems.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.utils.validation import check_array
-
+import pyDOE
+import matplotlib.pyplot as plt
 
 
 def Branin(x):
@@ -9,15 +10,13 @@ def Branin(x):
     # https://www.sfu.ca/~ssurjano/branin.html
 
     x = check_array(x)
-    x1 = x[:, 0]
-    x2 = x[:, 1]
+    x1 = np.atleast_2d(x[:, 0]).reshape(-1, 1)
+    x2 = np.atleast_2d(x[:, 1]).reshape(-1, 1)
 
     # assume that values in x is in range [0, 1]
-
-    if len(np.where(x > 1)) > 0 or len(np.where(x < 0)) > 0:
-        raise Exception('Brain input should be in range [0, 1]')
-        exit(-1)
-
+    if np.any(x > 1) or np.any(x < 0):
+        raise Exception('Branin input should be in range [0, 1]')
+        exit(1)
 
     x1 = -5 + (10 - (-5)) * x1
     x2 = 0 + (15 - 0) * x2
@@ -36,5 +35,28 @@ def Branin(x):
     f = part1 + part2 + part3
 
     return f
+
+
+if __name__ == "__main__":
+    lhs_x = pyDOE.lhs(2, 5)
+    z = Branin(lhs_x)
+    x = -5 + (10 - (-5)) * lhs_x[:, 0]
+    y = 0 + (15 - 0) * lhs_x[:, 1]
+
+    x_mesh, y_mesh = np.meshgrid(x, y)
+
+
+    fig = plt.figure(figsize=(6, 5))
+    left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+    ax = fig.add_axes([left, bottom, width, height])
+    cp = ax.contour(x_mesh, y_mesh, z)
+    ax.clabel(cp, inline=True,
+              fontsize=10)
+    ax.set_title('Contour Plot')
+    ax.set_xlabel('x (cm)')
+    ax.set_ylabel('y (cm)')
+    plt.show()
+
+
 
 
