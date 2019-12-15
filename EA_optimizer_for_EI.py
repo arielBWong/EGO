found where loglikelihood bug is

EA_optimizer_for_EI.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     multiprocessing.freeze_support()
 
     np.random.seed(10)
-    n_iter = 200
+    n_iter = 6
     func_val = {'next_x': 0}
 
     # === preprocess data change in each iteration of EI ===
@@ -298,6 +298,9 @@ if __name__ == "__main__":
         mean_train_x, std_train_x, norm_train_x = norm_data(train_x)
         mean_train_y, std_train_y, norm_train_y = norm_data(train_y)
         mean_cons_y, std_cons_y, norm_cons_y = norm_data(cons_y)
+
+        # if iteration == 3:
+        #    c = 0
 
         # use cross validation for hyper-parameter
         # the following is re-train from start

cross_val_hyperp.py
@@ -44,8 +44,8 @@ def external_optimizer(obj_func, initial_theta, bounds):
                                                                                bounds_ea,
                                                                                mut=0.8,
                                                                                crossp=0.7,
-                                                                               popsize=20,
-                                                                               its=20,
+                                                                               popsize=10,
+                                                                               its=10,
                                                                                **obj_args)
 
     theta_opt = pop_x[0, :]
@@ -134,7 +134,7 @@ def n_fold_cross_val_para(train_x, train_y, cons_y):
     # this is left for future change
     # yes first, let us shuffle the sample data
     index_samples = np.arange(n_samples)
-    np.random.shuffle(index_samples)
+    # np.random.shuffle(index_samples)
 
     train_x = train_x[index_samples, :]
     train_y = train_y[index_samples, :]